5e13cf1bd9b058f4bc73f03ecb32dd4a2e243a3a
Cody Partington <codythatsme@gmail.com>
2026-03-02 22:50:52 +1100

    Squashed 'vendors/Tautulli/' content from commit 0a837049c1
    git-subtree-dir: vendors/Tautulli
    git-subtree-split: 0a837049c1a62124087cca27f82d96603b75eede

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if [[ "$TAUTULLI_DOCKER" == "True" ]]; then
+    PUID=${PUID:-1000}
+    PGID=${PGID:-1000}
+
+    groupmod -o -g "$PGID" tautulli
+    usermod -o -u "$PUID" tautulli
+
+    find /config \! \( -uid $(id -u tautulli) -gid $(id -g tautulli) \) -print0 | xargs -0r chown tautulli:tautulli
+
+    echo "Running Tautulli using user tautulli (uid=$(id -u tautulli)) and group tautulli (gid=$(id -g tautulli))"
+    exec gosu tautulli "$@"
+else
+    python_versions=("python3.11" "python3.10" "python3.9" "python3" "python")
+    for cmd in "${python_versions[@]}"; do
+        if command -v "$cmd" >/dev/null; then
+            echo "Starting Tautulli with $cmd."
+            if [[ "$(uname -s)" == "Darwin" ]]; then
+                $cmd Tautulli.py &> /dev/null &
+            else
+                $cmd Tautulli.py --quiet --daemon
+            fi
+            exit
+        fi
+    done
+    echo "Unable to start Tautulli. No Python interpreter was found in the following options:" "${python_versions[@]}"
+fi
